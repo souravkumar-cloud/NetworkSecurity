f7541040b2aa781364800e671ce810b6f7d1aa4e
Bala <sokukumar678@gmail.com>
2026-01-21 17:33:47 +0530

    ETL pipeline is completed

---
+++ test_mongodb.py
@@ -0,0 +1,12 @@
+from pymongo.mongo_client import MongoClient
+import os
+
+uri = os.getenv("MONGO_DB_URL")
+
+client=MongoClient(uri)
+
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
